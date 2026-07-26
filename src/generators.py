c6cff696dc333068379bd0bf6d4c498dbe1373c8
Elena Trofimova-Pavlova <lessia7713@gmail.com>
2026-07-26 14:37:52 +0200

    Add generators module

---
+++ src/generators.py
@@ -0,0 +1,26 @@
+def filter_by_currency(transactions, currency):
+    """Возвращает генератор транзакций по указанной валюте."""
+
+    for transaction in transactions:
+        if transaction["operationAmount"]["currency"]["code"] == currency:
+            yield transaction
+
+
+def transaction_descriptions(transactions):
+    """Возвращает описания транзакций по одной."""
+
+    for transaction in transactions:
+        yield transaction["description"]
+
+
+def card_number_generator(start, stop):
+    """Генерирует номера банковских карт в формате XXXX XXXX XXXX XXXX."""
+
+    for number in range(start, stop + 1):
+        card_number = f"{number:016d}"
+        yield (
+            f"{card_number[:4]} "
+            f"{card_number[4:8]} "
+            f"{card_number[8:12]} "
+            f"{card_number[12:]}"
+        )
