fc2780c2c85a61713ae3b8062066094a2dbde35c
Elena Trofimova-Pavlova <lessia7713@gmail.com>
2026-07-31 11:59:07 +0200

    Add generators module and tests

---
+++ tests/test_generators.py
@@ -0,0 +1,47 @@
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+
+
+def test_filter_by_currency(operations):
+    usd_transactions = list(filter_by_currency(operations, "USD"))
+
+    assert len(usd_transactions) == 3
+    assert usd_transactions[0]["operationAmount"]["currency"]["code"] == "USD"
+    assert usd_transactions[1]["operationAmount"]["currency"]["code"] == "USD"
+    assert usd_transactions[2]["operationAmount"]["currency"]["code"] == "USD"
+
+
+def test_filter_by_currency_not_found(operations):
+    result = list(filter_by_currency(operations, "EUR"))
+
+    assert result == []
+
+def test_transaction_descriptions(operations):
+        descriptions = list(transaction_descriptions(operations))
+
+        assert descriptions == [
+            "Перевод организации",
+            "Перевод со счета на счет",
+            "Перевод со счета на счет",
+            "Перевод с карты на карту",
+            "Перевод организации",
+        ]
+
+def test_card_number_generator():
+    cards = list(card_number_generator(1, 3))
+
+    assert cards == [
+        "0000 0000 0000 0001",
+        "0000 0000 0000 0002",
+        "0000 0000 0000 0003",
+    ]
+
+def test_card_number_generator_one_number():
+    cards = list(card_number_generator(5, 5))
+
+    assert cards == ["0000 0000 0000 0005"]
+
+def test_card_number_generator_format():
+    card = next(card_number_generator(1, 1))
+
+    assert len(card) == 19
+    assert card == "0000 0000 0000 0001"
